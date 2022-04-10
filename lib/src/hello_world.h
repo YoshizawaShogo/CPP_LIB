41c85961737f3153c22758e61b043eab502fb063
Yoshizawa Shogo <shogo.yoshizawa.2000@gmail.com>
2022-04-10 10:54:22 +0900

    インクルードガードを大文字に変更

--- lib/src/hello_world.h
+++ lib/src/hello_world.h
@@ -1,4 +1,4 @@
-#ifndef hello_world_h
-#define hello_world_h
+#ifndef HELLO_WORLD_H
+#define HELLO_WORLD_H
 void hello_world(void);
 #endif
---
+++ lib/src/my_sort.h
@@ -0,0 +1,8 @@
+#ifndef MY_SORT_H_
+#define MY_SORT_H_
+
+namespace my_lib {
+
+}
+
+#endif
