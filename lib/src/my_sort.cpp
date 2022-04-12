828cea39f6149835c30653c4bc85451b1320be79
Yoshizawa Shogo <shogo.yoshizawa.2000@gmail.com>
2022-04-12 21:15:38 +0900

    色々なソート関数を作成するために、ランダムなvectorを生成する関数を作成

--- lib/src/my_sort.cpp
+++ lib/src/my_sort.cpp
@@ -1,5 +1,25 @@
 #include "my_sort.h"
 
 namespace my_lib {
-    
+    std::vector<int32_t> random_int_vector(uint32_t length, int32_t range_min, int32_t range_max, int32_t random_seed)
+    {
+        std::vector<int32_t> vec;
+        std::mt19937 rand_src(random_seed);
+        for(uint32_t i=0; i<length; ++i){
+            std::uniform_int_distribution<int32_t> rand_dist(range_min, range_max);
+            vec.push_back(rand_dist(rand_src));
+        }
+        return vec;
+    }
+
+    std::vector<double> random_double_vector(uint32_t length, double range_min, double range_max, int32_t random_seed)
+    {
+        std::vector<double> vec;
+        std::mt19937 rand_src(random_seed);
+        for(uint32_t i=0; i<length; ++i){
+            std::uniform_real_distribution<double> rand_dist(range_min, range_max);
+            vec.push_back(rand_dist(rand_src));
+        }
+        return vec;
+    }
 }
--- lib/src/my_sort.h
+++ lib/src/my_sort.h
@@ -1,8 +1,29 @@
 #ifndef MY_SORT_H_
 #define MY_SORT_H_
 
-namespace my_lib {
+#include <cstdint>
+#include <random>
+#include <iostream>
+#include <iomanip>
 
+namespace my_lib {
+    std::vector<int32_t> random_int_vector(uint32_t length, int32_t range_min, int32_t range_max, int32_t random_seed = 0);
+    std::vector<double> random_double_vector(uint32_t length, double range_min, double range_max, int32_t random_seed = 0);
+
+    template <typename T>
+    void show_iterator(T iter, uint32_t limited_col_max = 10, uint8_t num_width = 4)
+    {
+        uint32_t counter = 0;
+        for(auto x: iter){
+            std::cout << std::setw(num_width) << x << ", ";
+            counter += 1;
+            if(limited_col_max && counter == limited_col_max){
+                counter = 0;
+                std::cout << std::endl;
+            }
+        }
+        std::cout << std::endl;
+    }
 }
 
 #endif
